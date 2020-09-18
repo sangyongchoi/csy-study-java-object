33b2c92f4c5c8661bba397b1fc6dad0e9c72adcc
choi sang yong <csy7792@naver.com>
2020-09-19 01:24:49 +0900

    리팩토링

--- src/main/java/chapter_01/Theater.java
+++ src/main/java/chapter_01/Theater.java
@@ -1,21 +1,13 @@
 package chapter_01;
 
 public class Theater {
-    private TicketSeller ticketSeller;
+    private final TicketSeller ticketSeller;
 
     public Theater(TicketSeller ticketSeller) {
         this.ticketSeller = ticketSeller;
     }
 
     public void enter(Audience audience){
-        if(audience.getBag().hasInvitation()){
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        }else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
