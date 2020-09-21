1bc5169f6c1ad55d815d7e7c233a3b84ad4a4078
sangyongchoi <csy7792@naver.com>
2020-09-21 12:52:01 +0900

    add object chapter_2

---
+++ src/main/java/ObjectApplication.java
@@ -0,0 +1,14 @@
+import chapter_02.Money;
+import chapter_02.Movie;
+import chapter_02.NoneDiscountPolicy;
+
+import java.time.Duration;
+
+public class ObjectApplication {
+    public static void main(String[] args) {
+        Movie starWars = new Movie("스타워즈",
+                Duration.ofMinutes(210),
+                Money.wons(10000),
+                new NoneDiscountPolicy());
+    }
+}
---
+++ src/main/java/chapter_02/DiscountCondition.java
@@ -0,0 +1,5 @@
+package chapter_02;
+
+public interface DiscountCondition {
+    boolean isSatisfiedBy(Screening screening);
+}
