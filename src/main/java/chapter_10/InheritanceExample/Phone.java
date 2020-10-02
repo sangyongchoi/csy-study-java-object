1023023124b29dd89fc56996f6e2c4ec9e2854a8
choi sang yong <csy7792@naver.com>
2020-10-02 15:25:47 +0900

    change class name

--- src/main/java/chapter_10/InheritanceExample/Phone.java
+++ src/main/java/chapter_10/InheritanceExample/Phone.java
@@ -3,43 +3,21 @@ package chapter_10.InheritanceExample;
 import chapter_02.Money;
 import chapter_10.Call;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Phone {
-    private Money amount;
-    private Duration seconds;
+public abstract class Phone {
     private List<Call> calls = new ArrayList<>();
-
-    public Phone(Money amount, Duration seconds) {
-        this.amount = amount;
-        this.seconds = seconds;
-    }
-
-    public void call(Call call) {
-        calls.add(call);
-    }
-
-    public List<Call> getCalls() {
-        return calls;
-    }
-
-    public Money getAmount(){
-        return amount;
-    }
-
-    public Duration getSeconds() {
-        return seconds;
-    }
 
     public Money calculateFee(){
         Money result = Money.ZERO;
 
         for (Call call : calls) {
-            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            result.plus(calculateCallFee(call));
         }
 
         return result;
     }
+
+    abstract protected Money calculateCallFee(Call call);
 }
